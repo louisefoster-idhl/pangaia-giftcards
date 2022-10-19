364328c8c37b9addccd1f15aca61f71390546748
Louise Foster <louise-f@fostr.io>
2022-10-19 22:19:37 +0100

    changed graphql to node js

--- functions/index.js
+++ functions/index.js
@@ -1,48 +1,52 @@
 import fetch from 'node-fetch'
+import Shopify from '@shopify/shopify-api';
 
-const apiKey = process.env.API_KEY || ''
+const accessToken = process.env.API_KEY || ''
 const storeUrl = process.env.STORE_URL
 const apiVersion = process.env.API_VERSION
 
 exports.handler = async (event, context) => {
   const data = JSON.parse(event.body)
 
-  const graphql = async gql => {
-    const res = await fetch(
-      `https://${storeUrl}/admin/api/${apiVersion}/graphql.json`,
-      {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/graphql',
-          'X-Shopify-Access-Token': apiKey
-        },
-        body: `{
-          "query": "mutation discountCodeBasicCreate($basicCodeDiscount: DiscountCodeBasicInput!) { discountCodeBasicCreate(basicCodeDiscount: $basicCodeDiscount) { codeDiscountNode { codeDiscount { ... on DiscountCodeBasic { title codes(first: 10) { nodes { code } } startsAt endsAt customerSelection { ... on DiscountCustomerAll { allCustomers } } customerGets { value { ... on DiscountPercentage { percentage } } items { ... on AllDiscountItems { allItems } } } appliesOncePerCustomer } } } userErrors { field code message } } }",
-           "variables": {
-              "basicCodeDiscount": {
-                "title": "20% off all items during the summer of 2022",
-                "code": "SUMMER20",
-                "startsAt": "2022-06-21T00:00:00Z",
-                "endsAt": "2022-09-21T00:00:00Z",
-                "customerSelection": {
-                  "all": true
-                },
-                "customerGets": {
-                  "value": {
-                    "percentage": 0.2
-                  },
-                  "items": {
-                    "all": true
-                  }
-                },
-                "appliesOncePerCustomer": true
-              }
-            }
-          }`
-      }
-    )
-    return await res.json()
-  }
+  // const graphql = async gql => {
+  //   const res = await fetch(
+  //     `https://${storeUrl}/admin/api/${apiVersion}/graphql.json`,
+  //     {
+  //       method: 'POST',
+  //       headers: {
+  //         'Content-Type': 'application/graphql',
+  //         'X-Shopify-Access-Token': apiKey
+  //       },
+  //       body: '{
+  //         "query": "mutation discountCodeBasicCreate($basicCodeDiscount: DiscountCodeBasicInput!) { discountCodeBasicCreate(basicCodeDiscount: $basicCodeDiscount) { codeDiscountNode { codeDiscount { ... on DiscountCodeBasic { title codes(first: 10) { nodes { code } } startsAt endsAt customerSelection { ... on DiscountCustomerAll { allCustomers } } customerGets { value { ... on DiscountPercentage { percentage } } items { ... on AllDiscountItems { allItems } } } appliesOncePerCustomer } } } userErrors { field code message } } }",
+  //         "variables": {
+  //           "basicCodeDiscount": {
+  //             "title": "20% off all items during the summer of 2022",
+  //             "code": "SUMMER20",
+  //             "startsAt": "2022-06-21T00:00:00Z",
+  //             "endsAt": "2022-09-21T00:00:00Z",
+  //             "customerSelection": {
+  //               "all": true
+  //             },
+  //             "customerGets": {
+  //               "value": {
+  //                 "percentage": 0.2
+  //               },
+  //               "items": {
+  //                 "all": true
+  //               }
+  //             },
+  //             "appliesOncePerCustomer": true
+  //           }
+  //         }
+  //       }'
+  //     }
+  //   )
+  //   return await res.json()
+  // }
+
+  const client = new Shopify.Clients.Graphql(storeUrl, accessToken);
+
   const put = async (endpoint, data) => {
     const res = await fetch(
       `https://${storeUrl}/admin/api/${apiVersion}/${endpoint}`,
@@ -60,8 +64,71 @@ exports.handler = async (event, context) => {
 
   console.log(data)
 
-
-  const newDiscount = await graphql()
+  const newDiscount = await client.query({
+    data: {
+      "query": `mutation discountCodeBasicCreate($basicCodeDiscount: DiscountCodeBasicInput!) {
+        discountCodeBasicCreate(basicCodeDiscount: $basicCodeDiscount) {
+          codeDiscountNode {
+            codeDiscount {
+              ... on DiscountCodeBasic {
+                title
+                codes(first: 10) {
+                  nodes {
+                    code
+                  }
+                }
+                startsAt
+                endsAt
+                customerSelection {
+                  ... on DiscountCustomerAll {
+                    allCustomers
+                  }
+                }
+                customerGets {
+                  value {
+                    ... on DiscountPercentage {
+                      percentage
+                    }
+                  }
+                  items {
+                    ... on AllDiscountItems {
+                      allItems
+                    }
+                  }
+                }
+                appliesOncePerCustomer
+              }
+            }
+          }
+          userErrors {
+            field
+            code
+            message
+          }
+        }
+      }`,
+      "variables": {
+        "basicCodeDiscount": {
+          "title": "20% off all items during the summer of 2022",
+          "code": "SUMMER20",
+          "startsAt": "2022-06-21T00:00:00Z",
+          "endsAt": "2022-09-21T00:00:00Z",
+          "customerSelection": {
+            "all": true
+          },
+          "customerGets": {
+            "value": {
+              "percentage": 0.2
+            },
+            "items": {
+              "all": true
+            }
+          },
+          "appliesOncePerCustomer": true
+        }
+      },
+    },
+  });
   console.log(newDiscount)
 
   return {
